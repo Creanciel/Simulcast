df35e5987998073a3b5e67b849d08262fcf8c291
Creanciel <creanciel@gmail.com>
2025-11-06 01:30:23 +0900

    Add: simple page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div />
+  <div class="flex w-full h-full justify-center items-center">Hello</div>
 </template>
 
 <style scoped lang="css"></style>
